47a9bfbd29ed041bb371ec6c2a4ad9c2535dbda1
Victor Ribeiro <victorqribeiro@gmail.com>
2019-10-19 11:47:14 -0300

    add genetics and format code q

--- js/Bullet.js
+++ js/Bullet.js
@@ -1,49 +1,95 @@
 class Bullet {
 
-	constructor(x, y, size, angle, speed, targets = []){
+	constructor(owner, x, y, size, angle, speed, damage, targets = []){
+	
 		this.pos = {
+		
 			x: x || 0,
+			
 			y: y || 0
+			
 		};
+		
+		this.owner = owner || null;
+		
 		this.size = size || 5;
+		
 		this.angle = angle || 0;
+		
 		this.speed = speed || 0;
+		
+		this.damage = damage || 1;
+		
 		this.isGone = false;
+		
 		this.targets = targets;
+		
+	}
 
-	}
-	
+
 	update(){
+	
+		if( this.isGone )
+		
+			return
+	
+	
 		if( this.pos.x < -this.size || this.pos.y < -this.size || this.pos.x > w+this.size || this.pos.y > h+this.size ){
+		
 			this.isGone = true;
+			
 			return;
+			
 		}
+		
 		this.pos.x += Math.cos(this.angle) * this.speed * deltaTime;
+		
 		this.pos.y += Math.sin(this.angle) * this.speed * deltaTime;
+		
 		for(let i = this.targets.length-1; i >= 0; i--){
+		
+				if(this.targets[i].isDead)
+				
+					continue
+					
 				if( this.distance( this.targets[i] ) < this.targets[i].size+this.size ){
+				
+					if(this.owner) this.owner.hits++;
+					
 					this.targets[i].speed.x += Math.cos(this.angle) * 0.1;
+					
 					this.targets[i].speed.y += Math.sin(this.angle) * 0.1;
-					this.targets[i].health -= 1;
-					if( this.targets[i].health < 1 ){
-						this.targets[i].isDead = true;
-						this.targets.splice(i, 1)
-					}
+					
+					this.targets[i].health -= this.damage;
+					
 					this.isGone = true;
+					
 					break;
+					
 				}
+				
 		}
+		
 	}
-	
+
+
 	distance(target){
-		return Math.abs(this.pos.x - target.pos.x) + Math.abs(this.pos.y - target.pos.y)
-	}
 	
+		return Math.abs(this.pos.x - target.pos.x) + Math.abs(this.pos.y - target.pos.y);
+		
+	}
+
+
 	show(){
+	
 		c.fillStyle = "black";
-		c.beginPath()
+		
+		c.beginPath();
+		
 		c.arc(this.pos.x, this.pos.y, this.size, 0, TWOPI);
+		
 		c.fill();
+		
 	}
 	
 }
--- js/Player.js
+++ js/Player.js
@@ -1,91 +1,175 @@
 class Player {
 
 	constructor(x, y, angle, ia = false){
+	
 		this.pos = {
+		
 			x: x || w2,
+			
 			y: y || h2
+			
 		};
+		
 		this.health = 10;
+		
 		this.angle = angle || 0;
+		
 		this.ia = ia;
+		
 		this.size = 30;
+		
 		this.looking = {
+		
 			x: w2,
+			
 			y: h2
+			
 		}
+		
 		this.isMoving = {
+		
 			left: false,
+			
 			up: false,
+			
 			right: false,
+			
 			down: false
+			
 		}
+		
 		this.isShooting = false;
+		
 		this.velocity = 0.01;
+		
 		this.speed = {
+		
 			x: 0,
+			
 			y: 0
+			
 		};
+		
 		this.friction = 0.97;
+		
 		this.isDead = false;
-		this.coolDown = 10;
+		
+		this.coolDownInit = 15;
+		
+		this.coolDown = this.coolDownInit;
+		
 		this.iAnim = 0;
+		
+		this.shootsFired = 0;
+		
+		this.hits = 0;
+		
+		this.age = 0;
+		
 	}
 
+
 	lookAt(x, y){
+	
 		this.looking.x = x;
+		
 		this.looking.y = y;
+		
 	}	
+
 
 	update(input = null){
 	
 		if(this.isDead)
+		
 			return
+			
+		if(this.health < 1){
+		
+			this.isDead = true
+			
+			this.age = (Date.now() - startTime) / 1000;
+			
+			return
+			
+		}
 
 		if( this.ia )
+		
 			this.updateIA(input)
 
 		this.angle = Math.atan2( this.looking.y - this.pos.y, this.looking.x - this.pos.x );
+		
 		if( this.isMoving.left )
+		
 			this.speed.x -= this.velocity;
+			
 		if( this.isMoving.up )
+		
 			this.speed.y -= this.velocity;
+			
 		if( this.isMoving.right )
+		
 			this.speed.x += this.velocity;
+			
 		if( this.isMoving.down )
+		
 			this.speed.y += this.velocity;
 	
 		const _x = this.speed.x * deltaTime;
+		
 		const _y = this.speed.y * deltaTime;
 
 		if( this.pos.x + _x > 0 && this.pos.x + _x < w )	
+		
 			this.pos.x += _x;
+			
 		else
+		
 			this.speed.x = -this.speed.x
 			
 		if( this.pos.y + _y > 0 && this.pos.y + _y < h )	
+		
 			this.pos.y += _y;
+			
 		else
+		
 			this.speed.y = -this.speed.y
 			
 		this.speed.x *= this.friction;
+		
 		this.speed.y *= this.friction;
 		
 		if( this.isShooting && this.coolDown < 1){
-			this.coolDown = 5;
+		
+			this.coolDown = this.coolDownInit;
+			
 			this.isShooting = false;
+			
 			const targets = players.slice( 0 );
-			targets.splice( targets.indexOf(this), 1)
+			
+			targets.splice( targets.indexOf(this), 1);
+			
 			bullets.push( 
-				new Bullet( this.pos.x + Math.cos(this.angle) * 40, this.pos.y + Math.sin(this.angle) * 40, 5, this.angle, 1.2, targets) 
+			
+				new Bullet( this, this.pos.x + Math.cos(this.angle) * 40, this.pos.y + Math.sin(this.angle) * 40, 5, this.angle, 1.2, 1, targets) 
+				
 			);
+			
+			this.shootsFired++;
+			
 		}
 		
-		this.coolDown--
+		this.coolDown--;
 
 	}
+
 	
 	updateIA(target){
-		const action = this.brain.predict( [target.pos.x / w, target.pos.y / h, target.looking.x / w, target.looking.y / h, target.isShooting ? 1 : 0 ] ).data;
+	
+		const action = this.brain.predict( 
+			[target.pos.x / w, target.pos.y / h, target.looking.x / w, target.looking.y / h, target.isShooting ? 1 : 0 ] 
+		).data;
 		
 		action[0] > 0.5 ? this.isMoving.left = true : this.isMoving.left = false;
 			
@@ -102,33 +186,60 @@ class Player {
 		action[6] > 0.5 ? this.isShooting = true : this.isShooting = false;
 		
 	}
+
 	
 	show(){
+	
 		if( this.isDead ){
+		
 			this.iAnim += 0.1
+			
 			c.fillStyle = "rgba(0,0,0,"+(1-this.iAnim)+")";
+			
 			c.beginPath();
+			
 			c.arc(this.pos.x, this.pos.y, this.size, 0, TWOPI);
+			
 			c.fill();
+			
 			c.save();
+			
 			c.translate(this.pos.x, this.pos.y);
+			
 			c.rotate(this.angle+this.iAnim);
+			
 			c.fillRect(this.iAnim*50, -9, 50, 18);
+			
 			c.restore();
+			
 			return
+			
 		}
+		
 		c.fillStyle = "red";
+		
 		c.fillRect(this.pos.x - 50, this.pos.y - 60, this.health * 10 , 10);
+		
 		c.strokeRect(this.pos.x - 50, this.pos.y - 60, 100, 10);
+		
 		c.fillStyle = "black";
+		
 		c.beginPath();
+		
 		c.arc(this.pos.x, this.pos.y, this.size, 0, TWOPI);
+		
 		c.fill();
+		
 		c.save();
+		
 		c.translate(this.pos.x, this.pos.y);
+		
 		c.rotate(this.angle);
+		
 		c.fillRect(0, -9, 50, 18);
+		
 		c.restore();
+		
 	}
 
 }
